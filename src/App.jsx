f64343b9dfd1b09cf236811108b336f91b2d9561
Nicolas Towery <nicolastowery@gmail.com>
2023-11-03 16:54:32 -0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,32 @@ import { fetchVerse } from "./services/esvApi";
 import { useEffect, useState } from "react";
 import "./index.css";
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
   const [passage, setPassage] = useState("");
   const [address, setAddress] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
   const handleNewVerse = async (e) => {
     e && e.preventDefault();
     setIsLoading(true);
-    const { book, address } = verses[Math.floor(Math.random() * verses.length)];
-    const { passages, canonical } = await fetchVerse(book, address);
-    setPassage(passages);
-    setAddress(canonical);
-    setIsLoading(false);
   };
 
-  // Load verse on component mount
   useEffect(() => {
-    handleNewVerse();
-  }, []);
+    if (isLoading) {
+      const { book, address } =
+        verses[Math.floor(Math.random() * verses.length)];
+      fetchVerse(book, address).then(({ passages, canonical }) => {
+        setPassage(passages[0]);
+        setAddress(canonical);
+        setIsLoading(false);
+      });
+    }
+  }, [isLoading]); // Only fetch verse when isLoading changes
+
+  useEffect(() => {
+    if (!isLoading) {
+      handleNewVerse();
+    }
+  }, []); // Fetch the first verse on component mount
 
   return (
     <div className="app-container">
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,9 +1,41 @@
+import { useRef } from "react";
+const regex = /[a-zA-Z0-9]+(?![^[]*])\b/g;
 function Quiz({ passage, address }) {
+  const words = passage.match(regex);
+  const segments = passage.trim().split(/—| /);
+  // const wordIndecies = useRef([]);
+  const currentArray = [];
+  if (words) {
+    for (let i = 0; i < 3; i++) {
+      let randomWordIndex;
+      let containsWordIndex = false;
+      randomWordIndex = Math.floor(Math.random() * words.length);
+      currentArray.length > 0 &&
+        currentArray.forEach((word) => {
+          if (word.index === randomWordIndex) {
+            containsWordIndex = true;
+          }
+        });
+      if (!containsWordIndex) {
+        const randomWord = {
+          index: randomWordIndex,
+          text: words[randomWordIndex],
+        };
+        currentArray.push(randomWord);
+      }
+    }
+    console.log(currentArray);
+  }
   return (
     <div>
+      quiz
       <div>{address}</div>
       <br />
-      <div>{passage}</div>
+      <div>
+        {currentArray.map((word) => {
+          return <span>{word.text} </span>;
+        })}
+      </div>
     </div>
   );
 }
