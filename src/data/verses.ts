c1f656dd9d6fc804378b337210d2271cf8a62906
Nicolas Towery <nicolastowery@gmail.com>
2024-02-06 10:22:21 -0600

    adjusted mobile height

--- src/data/verses.ts
+++ src/data/verses.ts
@@ -3,14 +3,14 @@ const verses = [
   { book: "Psalm", address: "119:11" },
   { book: "John", address: "3:30" },
   { book: "Romans", address: "8:39" },
-  // { book: "Psalm", address: "139:23-24" },
-  // { book: "Matthew", address: "10:28" },
-  // { book: "John", address: "1:1-3" },
-  // { book: "Romans", address: "3:23-24" },
-  // { book: "Romans", address: "5:6-8" },
-  // { book: "Philipians", address: "4:6-7" },
-  // { book: "2Timothy", address: "3:16-17" },
-  // { book: "Hebrews", address: "4:15" },
+  { book: "Psalm", address: "139:23-24" },
+  { book: "Matthew", address: "10:28" },
+  { book: "John", address: "1:1-3" },
+  { book: "Romans", address: "3:23-24" },
+  { book: "Romans", address: "5:6-8" },
+  { book: "Philipians", address: "4:6-7" },
+  { book: "2Timothy", address: "3:16-17" },
+  { book: "Hebrews", address: "4:15" },
 ];
 
 export default verses;
